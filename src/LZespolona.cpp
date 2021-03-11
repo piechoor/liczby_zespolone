a1f40180dd61ab559be4a8b551f0b419c5baeb2b
jzajac <jzajac@asus.home>
2021-03-11 21:51:51 +0100

    initial commit

---
+++ src/LZespolona.cpp
@@ -0,0 +1,22 @@
+#include "LZespolona.hh"
+#include <cassert>
+
+
+/*!
+ * Realizuje dodanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik dodawania,
+ *    Skl2 - drugi skladnik dodawania.
+ * Zwraca:
+ *    Sume dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+  
+  assert((Skl1.re != 0) || (Skl2.im !=0));
+
+  Wynik.re = Skl1.re + Skl2.re;
+  Wynik.im = Skl1.im + Skl2.im;
+  return Wynik;
+}
